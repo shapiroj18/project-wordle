2bab4cf1e81db632301342bb9ecfd8355c276fca
Jonathan Shapiro <55108156+shapiroj18@users.noreply.github.com>
2024-02-27 18:15:52 -0500

    Exercise 4 (#4)

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,16 +1,32 @@
 import React from "react";
 
+import { checkGuess } from "../../game-helpers";
 import { range } from "../../utils";
 
-function Guess({ word }) {
+function Guess({ word, answer }) {
+  const checkValues = checkGuess(word, answer);
+  function getStatusClass(position) {
+    const status = checkValues[position].status;
+    if (status === "incorrect") {
+      return "cell incorrect";
+    } else if (status === "correct") {
+      return "cell correct";
+    } else if (status === "misplaced") {
+      return "cell misplaced";
+    } else throw new Error("Not a valid status");
+  }
   return (
     <>
       <p className="guess">
-        {range(5).map((num) => (
-          <span key={num} className="cell">
-            {word ? word[num] : undefined}
-          </span>
-        ))}
+        {range(5).map((num) =>
+          word ? (
+            <span key={num} className={getStatusClass(num)}>
+              {word[num]}
+            </span>
+          ) : (
+            <span key={num} className="cell"></span>
+          )
+        )}
       </p>
     </>
   );
